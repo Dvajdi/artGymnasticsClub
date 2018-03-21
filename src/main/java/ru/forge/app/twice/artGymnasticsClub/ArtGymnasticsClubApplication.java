b9a15cb141dbca5ed062113c33bef27dde2685c2
Twice <dvajdi@bk.ru>
2018-03-21 21:44:19 +0300

    start Project

---
+++ src/main/java/ru/forge/app/twice/artGymnasticsClub/ArtGymnasticsClubApplication.java
@@ -0,0 +1,12 @@
+package ru.forge.app.twice.artGymnasticsClub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtGymnasticsClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtGymnasticsClubApplication.class, args);
+	}
+}
---
+++ src/test/java/ru/forge/app/twice/artGymnasticsClub/ArtGymnasticsClubApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.forge.app.twice.artGymnasticsClub;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArtGymnasticsClubApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
