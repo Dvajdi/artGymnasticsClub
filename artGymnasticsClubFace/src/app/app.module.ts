ccfdbab538f663544b9bde9ec518fc1ae04723d0
Twice <dvajdi@bk.ru>
2018-03-25 13:11:56 +0300

    add bootstrap and jquery; add skeleton of frontend

--- artGymnasticsClubFace/src/app/app.module.ts
+++ artGymnasticsClubFace/src/app/app.module.ts
@@ -2,10 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainContainerComponent } from './main-container/main-container.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './main-container/header/header.component';
+import { HeadlineComponent } from './main-container/headline/headline.component';
+import { GaleryComponent } from './main-container/galery/galery.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainContainerComponent,
+    FooterComponent,
+    HeaderComponent,
+    HeadlineComponent,
+    GaleryComponent
   ],
   imports: [
     BrowserModule
